f27015fa8043d35a4d760bd159f78406c401e681
mizuame60214 <m011903059@edu.teu.ac.jp>
2022-01-29 11:30:07 +0900

    スプーンフォーク切り替えと、スイーツ作りました！

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
     public float speed = 1.0f;
+    //ゲームが終わってるか終わってないか
+    bool gameMasFinishBool;
+    private Rigidbody rb;
 
-    // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        gameMasFinishBool = GameObject.Find("GameMaster").GetComponent<GameMaster>().gameFinish;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        //ゲームが終わったら動かなくなる。
+        gameMasFinishBool = GameObject.Find("GameMaster").GetComponent<GameMaster>().gameFinish;
+        if (gameMasFinishBool) return;
+
+        //指定したスピードから現在の速度を引いて加速力を求める
+        float currentSpeed = speed - rb.velocity.magnitude;
+        //調整された加速力で力を加える
+        rb.AddForce(new Vector3(0, 0, currentSpeed));
     }
 }
