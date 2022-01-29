c0f825d9706c7492a3acde1a0955d019f53b2d0a
MSI\yukino <3119067@nagoya-bunri.ac.jp>
2022-01-29 19:16:12 +0900

    Merge branch 'main' of https://github.com/sukeU/new_Amateur_Eater_club into nomura

---
+++ Assets/sukeU/Scripts/createParticles.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class createParticles : MonoBehaviour
+{
+    [SerializeField]
+    GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(prefab, transform.position, Quaternion.identity);
+        Instantiate(prefab, transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
