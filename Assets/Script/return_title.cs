b615ce67be0ee357b9731e1f3b89e3f6cc83ee79
nishiki8845 <yoshiki95778@gmail.com>
2022-01-30 09:37:26 +0900

    ボタンを押してタイトルに戻るように

---
+++ Assets/Script/return_title.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;//シーンをロードする場合に必要
+
+public class return_title : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnRetry()
+    {
+        SceneManager.LoadScene("title");
+    }
+}
