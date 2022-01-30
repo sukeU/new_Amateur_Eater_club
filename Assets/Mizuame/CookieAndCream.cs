2066e5abc24386df1f5056b23b677594b89b0445
mizuame214 <m011903059@edu.teu.ac.jp>
2022-01-30 16:31:27 +0900

    クッキーの床にしました。

---
+++ Assets/Mizuame/CookieAndCream.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookieAndCream : MonoBehaviour
+{
+    GameObject prefab;
+    public float kankaku = 2.1f;
+    public int num = 2;
+
+    void Start()
+    {
+        prefab = transform.Find("CookieAndCream").gameObject;
+        for (int i = 1; i < num + 1; i++)
+        {
+            Instantiate(prefab, new Vector3(prefab.transform.position.x, prefab.transform.position.y, prefab.transform.position.z + kankaku * (float)i), Quaternion.identity, this.transform);
+        }
+        Destroy(this);
+    }
+}
