dca420a5ce6a7d3e1828d3c5111ac378a44ccf54
2023150-PC\2023150 <daigakukait2023@gmail.com>
2022-01-30 14:04:51 +0900

    クレジット以外の遷移ができるようになりました

--- Assets/Script/return_title.cs
+++ Assets/Script/return_title.cs
@@ -9,6 +9,6 @@ public class return_title : MonoBehaviour
     // Start is called before the first frame update
     public void OnRetry()
     {
-        SceneManager.LoadScene("title");
+        SceneManager.LoadScene("Title");
     }
 }
